d81ac68c1d49499884886a66284086b866c4a098
Adnan Hakak <adnanshowkat@gmail.com>
2021-10-25 19:55:50 +0530

    variable change

--- crypto.js
+++ crypto.js
@@ -6,8 +6,8 @@ let textbox = document.querySelector('#textbox')              //get textbox
 const getPrice = async () => {
     let textboxValue = textbox.value;
     let resolvedPromise = await axios.get(`https://api.cryptonator.com/api/ticker/${textboxValue}-usd`)   //send req to api
-    console.log(resolvedPromise.data.ticker.price)                                                        //fxn will return price
-    return resolvedPromise.data.ticker.price;
+    return resolvedPromise.data.ticker.price;                                                   //fxn will return price
+    console.log(resolvedPromise.data.ticker.price)
 }
 
 // this is a test comment
@@ -16,11 +16,14 @@ button.addEventListener('click', createLi);
 
 
 async function createLi() {
-    let fetchedPrice = await getPrice();                                                             //23445
+    let fetchedPrice = await getPrice();                                                             //price from server
     let newLi = document.createElement("li");                                                         //<li></li>
-    newLi.append(`The price of ${textbox.value} at ${Date()} is '${fetchedPrice}'`);                  //<li>23445</li>
+    newLi.append(`The price of ${textbox.value} at ${getTime()} is '${fetchedPrice}'`);                  //<li>23445</li>
     ul.append(newLi);                                                                                  //<ul><li>23345</li></ul>
     console.log(textbox.value)
     textbox.value = '';
 }
 
+function getTime() {
+    return (new Date()).toLocaleTimeString()
+}
